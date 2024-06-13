192d197eb48eeedb372b8953f87199ed38bfa443
Assanali18 <ustaevasanali@gmail.com>
2024-06-14 01:24:14 +0500

    back

--- nf-chat-hw/backend/src/index.ts
+++ nf-chat-hw/backend/src/index.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: ['GET', 'POST']
   }
 });
