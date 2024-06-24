410826c8f6388321a01dc03561a8042e03ce47a2
Assanali18 <ustaevasanali@gmail.com>
2024-06-24 05:59:45 +0500

    fix cors

--- nf-hw-ai-1/backend/src/index.ts
+++ nf-hw-ai-1/backend/src/index.ts
@@ -22,7 +22,7 @@ const server = http.createServer(app);
 server.on('upgrade', (request, socket, head) => {
   const origin = request.headers.origin;
 
-  if (origin === 'http://localhost:3000') {
+  if (origin === 'https://chat-ai-gamma-indol.vercel.app') {
     wss.handleUpgrade(request, socket, head, (ws) => {
       wss.emit('connection', ws, request);
     });
